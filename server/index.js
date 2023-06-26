393211bfdda6e66d2f95fedbfe81dc90fe3b9f79
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-06-25 18:25:01 -0700

    input configurations for middleware

--- server/index.js
+++ server/index.js
@@ -10,3 +10,15 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 /* CONFIGURATIONS */
+const __filename = fileURLToPath(import.meta.url); // grabs file url from the modules so that we can use the following directory name
+
+const __dirname = path.dirname(__filename);
+dotenv.config();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
