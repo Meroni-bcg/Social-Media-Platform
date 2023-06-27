8f43888d3d3dc1e84bf6905ce54ddd1304c3681d
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-06-26 19:59:16 -0700

    updated users.js created posts.js routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,5 +4,14 @@ import {
     getUserFriends,
     addRemoveFriend,
 } from "../middleware/auth.js";
-
+import { verifyToken } from "../middleware/auth.js"; 
 const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
