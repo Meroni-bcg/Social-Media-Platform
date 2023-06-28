2ba7c71136f6fa7823cab328fbb49c3e54aca004
Meroni <114459394+Meroni-bcg@users.noreply.github.com>
2023-06-27 21:26:37 -0700

    updated index.js

--- server/index.js
+++ server/index.js
@@ -45,6 +45,7 @@ const upload = multer({ storage });
 /* ROUTES WITH FILES */
 app.post("/auth/register", upload.single("picture"), register);
 app.post("/posts", verifyToken, upload.single("picture"), createPost); 
+
 /* ROUTES */
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
